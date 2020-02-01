dc68461df147c89dbc2687dee5838d1697e63d14
Krishna Kant <lushkant@gmail.com>
2020-02-02 00:36:29 +0530

    Fix failing tests with filname function and multer storage

--- api/src/router.js
+++ api/src/router.js
@@ -3,11 +3,11 @@ const multer = require('multer');
 const router = Router();
 
 const filename = (request, file, callback) => {
-	callback(null, file.originalName);
+	callback(null, file.originalname);
 };
 
 const storage = multer.diskStorage({
-	destination: 'api/uploads',
+	destination: 'api/uploads/',
 	filename,
 });
 
@@ -26,11 +26,9 @@ const upload = multer({
 });
 
 router.post('/upload', upload.single('photo'), (request, response) => {
-	if (request.fileValidationError) return response.status(400).json({
-		error: request.fileValidationError
-	});
+	if (request.fileValidationError) return response.status(400).json({error: request.fileValidationError});
 
-	return response.status(201).json({ success: true });
+	return response.status(201).json({success: true});
 });
 
 module.exports = router;
